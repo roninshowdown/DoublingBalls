a96a10f3a5a90259299b373db74d77248c7dfb5c
DP1997 <donaldp78@googlemail.com>
2018-05-14 16:52:08 +0200

    fullscreen

--- app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
+++ app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
@@ -10,9 +10,6 @@ public class BubblesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bubble);
-        /*DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        Constants.SCREEN_WIDTH = dm.widthPixels;
-        Constants.SCREEN_HEIGHT = dm.heightPixels;*/
+
     }
 }
