0693b9c18c829566acd53278778cb7dc11b41b30
DP1997 <donaldp78@googlemail.com>
2018-05-14 16:40:20 +0200

    fullscreen

--- app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
+++ app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
@@ -1,11 +1,12 @@
 package com.example.donald.doublingballs;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class BubblesActivity extends AppCompatActivity {
+public class BubblesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
