a5f7d6016f6f07edcea752fab0bac63882dec599
roninshowdown <39188396+roninshowdown@users.noreply.github.com>
2018-05-15 09:17:35 +0200

    c

--- app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
+++ app/src/main/java/com/example/donald/doublingballs/BubblesActivity.java
@@ -18,5 +18,6 @@ public class BubblesActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_bubble);
+
     }
 }
