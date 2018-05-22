6927c4c7f639935892d30eafc601eeddae0987cb
roninshowdown <39188396+roninshowdown@users.noreply.github.com>
2018-05-22 19:35:40 +0200

    Merge branch 'master' of https://github.com/roninshowdown/DoublingBalls
    # Conflicts:
    #	app/src/main/java/com/example/donald/doublingballs/Sound.java
    #	app/src/main/res/drawable-v24/gameover.png
    #	app/src/main/res/drawable-v24/gameoverwithhighscore.png
    #	app/src/main/res/drawable-v24/menuactivity.png
    #	app/src/main/res/drawable-v24/shopactivity.png

--- app/src/main/java/com/example/donald/doublingballs/Score.java
+++ app/src/main/java/com/example/donald/doublingballs/Score.java
@@ -9,10 +9,17 @@ import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Score extends Activity {
 
+    Button shareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,31 +31,57 @@ public class Score extends Activity {
 
 
         TextView currentscoreLabel = (TextView) findViewById(R.id.currentscoreLabel);
-        TextView highscoreLabel = (TextView) findViewById(R.id.highscoreLabel);
+        TextView highscoreLabel1 = (TextView) findViewById(R.id.highscoreLabel1);
+
 
         int currentscore = getIntent().getIntExtra("SCORE", 0);
         Log.d("currentScore: ", Integer.toString(currentscore));
         currentscoreLabel.setText(currentscore + "");
 
+        // saved Scores
         SharedPreferences settings = getSharedPreferences("GAME_DATA", Context.MODE_PRIVATE);
-        int highscore = settings.getInt("HIGH_SCORE", 0);
+        final int highscore = settings.getInt("HIGH_SCORE", 0);
 
-        if(currentscore > highscore){
-            highscoreLabel.setText("Highscore: " + currentscore);
+        if(currentscore > highscore) {
+            highscoreLabel1.setText("HighScore : " + currentscore);
+        }
 
             // Spielstand sichern
 
-            SharedPreferences.Editor editor = settings.edit();
-            editor.putInt("HIGH_SCORE", currentscore);
-            editor.commit();
-        }
-        else{
-            highscoreLabel.setText("High Score : " + highscore);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt("HIGH_SCORE", currentscore);
+        editor.commit();
+
+
+            shareButton = (Button) findViewById(R.id.share);
+            shareButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Intent.ACTION_SEND);
+                    i.setType("text/plain");
+                    String shareBody = "Highscore Doubling Balls: " + highscore + " Punkte !";
+                    String shareSub = "Doubling Balls Highscore";
+                    i.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+                    i.putExtra(Intent.EXTRA_TEXT, shareBody);
+                    startActivity(Intent.createChooser(i,"Highscore teilen"));
+                }
+            });
         }
 
-    }
 
     public void tryAgain(View view){
         startActivity(new Intent(getApplicationContext(), BubblesActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 }
+
